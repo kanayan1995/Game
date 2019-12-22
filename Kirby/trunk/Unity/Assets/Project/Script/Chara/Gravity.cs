df64a5e5d719492d48f731a2e09155300df4bd2b
kanayan0801 <kanayan0801@gmail.com>
2019-12-23 08:01:54 +0900

    重力操作Script作成

---
+++ Kirby/trunk/Unity/Assets/Project/Script/Chara/Gravity.cs
@@ -0,0 +1,24 @@
+﻿/**
+ *  @brief  重力設定
+ *  @file   yokoe-asuka
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 重力設定
+/// </summary>
+public class Gravity : MonoBehaviour
+{
+    private const float GRAVITY = 1.0f; // 重力設定
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void Update()
+    {
+        transform.position += new Vector3(0.0f, -GRAVITY, 0.0f) * Time.deltaTime;
+    }
+}
