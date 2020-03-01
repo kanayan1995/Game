e6f9a2adbb81d627f7530ec58cafcc50a231392b
kanayan0801 <kanayan0801@gmail.com>
2020-03-02 07:36:31 +0900

    // ベースキャラの作成

---
+++ Kirby/trunk/Unity/Assets/Project/Script/Chara/BaseCharacter/BaseCharacter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacter : MonoBehaviour
+{
+    // 移動計算
+    private Vector3 position;   //　位置
+    private Vector3 direction;  //  向き
+    private Vector3 move;       //  動値
+    private Vector3 acceler;    //  加速度
+    private float friction;     //  摩擦
+
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void Update()
+    {
+        
+    }
+}
